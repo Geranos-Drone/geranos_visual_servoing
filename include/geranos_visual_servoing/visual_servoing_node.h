6782a93fe68afeda90f619673dd1086cdf63de02
TimReX-22 <tim.reinhart@bluewin.ch>
2022-06-14 15:22:46 +0200

    added trajectory planer and TFs

--- include/geranos_visual_servoing/visual_servoing_node.h
+++ include/geranos_visual_servoing/visual_servoing_node.h
@@ -6,6 +6,16 @@
 #include <mav_msgs/common.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <mav_planning_msgs/PolynomialTrajectory.h>
+
+// Trajectories
+#include <mav_trajectory_generation/trajectory.h>
+#include <mav_trajectory_generation/polynomial_optimization_nonlinear.h>
+#include <mav_trajectory_generation_ros/ros_conversions.h>
+
+//tf
+#include <tf/transform_listener.h>
+#include <tf_conversions/tf_eigen.h>
 
 namespace geranos {
 	class VisualServoingNode {
@@ -16,18 +26,41 @@ namespace geranos {
 		void odometryCallback(const nav_msgs::OdometryConstPtr& odometry_msg);
 		void poseEstimateCallback(const geometry_msgs::PoseStamped::ConstPtr& pose_msg);
 
+		void loadParams();
+		void loadTFs();
+
 		void transformPose();
 		void transformOdometry();
+
+		bool planTrajectory(const Eigen::VectorXd& goal_pos,
+	                          const Eigen::VectorXd& goal_vel,
+	                          const Eigen::VectorXd& start_pos,
+	                          const Eigen::VectorXd& start_vel,
+	                          double v_max, double a_max,
+	                          mav_trajectory_generation::Trajectory* trajectory);
+		void publishTrajectory(const mav_trajectory_generation::Trajectory& trajectory);
 
 		ros::NodeHandle nh_;
 		ros::NodeHandle private_nh_;
 
 		ros::Subscriber odometry_sub_;
 		ros::Subscriber pose_estimate_sub_;
+		ros::Publisher pub_trajectory_;
+
+		tf::TransformListener tf_listener_;
+	  	tf::StampedTransform tf_base_imu_;
+	  	tf::StampedTransform tf_base_cam_;
+	  	Eigen::Affine3d T_B_imu_;
+	  	Eigen::Affine3d T_B_cam_;
 
 		mav_msgs::EigenOdometry current_odometry_;
 		Eigen::Vector3d current_pole_pos_;
 		Eigen::Quaterniond current_pole_rot_;
+
+		double max_v_; // m/s
+		double max_a_; // m/s^2
+		double max_ang_v_;
+		double max_ang_a_;
 
 	};
 }
--- src/visual_servoing_node.cpp
+++ src/visual_servoing_node.cpp
@@ -6,6 +6,9 @@ namespace geranos {
     private_nh_(private_nh) {
       odometry_sub_ = nh_.subscribe(mav_msgs::default_topics::ODOMETRY, 1, &VisualServoingNode::odometryCallback, this);
       pose_estimate_sub_ = nh_.subscribe("PolePoseNode/EstimatedPose", 1, &VisualServoingNode::poseEstimateCallback, this);
+      pub_trajectory_ = nh_.advertise<mav_planning_msgs::PolynomialTrajectory>(ros::this_node::getName() + "/trajectory", 0);
+      loadParams();
+      loadTFs();
     }
 
   VisualServoingNode::~VisualServoingNode() {}
@@ -20,6 +23,119 @@ namespace geranos {
     current_pole_rot_ = mav_msgs::quaternionFromMsg(pose_msg->pose.orientation);
   }
 
+  void VisualServoingNode::loadParams() {
+    if (!nh_.getParam(ros::this_node::getName() + "/max_v", max_v_)){
+      ROS_WARN("[VisualServoingNode] param max_v not found");
+    }
+    if (!nh_.getParam(ros::this_node::getName() + "/max_a", max_a_)){
+      ROS_WARN("[VisualServoingNode] param max_a not found");
+    }
+    if (!nh_.getParam(ros::this_node::getName() + "/max_ang_v", max_ang_v_)){
+      ROS_WARN("[VisualServoingNode] param max_ang_v not found");
+    }
+    if (!nh_.getParam(ros::this_node::getName() + "/max_ang_a", max_ang_a_)){
+      ROS_WARN("[VisualServoingNode] param max_ang_a not found");
+    }
+  }
+
+  void VisualServoingNode::loadTFs() {
+    ROS_INFO_ONCE("[VisualServoingNode] loading TFs");
+    try
+      {
+        tf_listener_.waitForTransform("base", "imu", ros::Time(0),
+                                      ros::Duration(5.0));
+        tf_listener_.lookupTransform("base", "imu",  
+                                    ros::Time(0), tf_base_imu_);
+        tf::transformTFToEigen(tf_base_imu_, T_B_imu_);
+        ROS_INFO_STREAM("[VisualServoingNode] Found base to imu transform!");
+      }
+    catch (tf::TransformException ex)
+      {
+        ROS_ERROR("%s",ex.what());
+      }
+    try
+      {
+        tf_listener_.waitForTransform("base", "cam", ros::Time(0),
+                                      ros::Duration(5.0));
+        tf_listener_.lookupTransform("base", "cam",  
+                                    ros::Time(0), tf_base_cam_);
+        tf::transformTFToEigen(tf_base_cam_, T_B_cam_);
+        ROS_INFO_STREAM("[VisualServoingNode] Found base to cam transform!");
+      }
+    catch (tf::TransformException ex)
+      {
+        ROS_ERROR("%s",ex.what());
+      }
+  }
+
+  // Plans a trajectory from a start position and velocity to a goal position and velocity
+  bool VisualServoingNode::planTrajectory(const Eigen::VectorXd& goal_pos,
+                                      const Eigen::VectorXd& goal_vel,
+                                      const Eigen::VectorXd& start_pos,
+                                      const Eigen::VectorXd& start_vel,
+                                      double v_max, double a_max,
+                                      mav_trajectory_generation::Trajectory* trajectory) {
+    assert(trajectory);
+    const int dimension = goal_pos.size();
+    assert(dimension == 3);
+    // Array for all waypoints and their constraints
+    mav_trajectory_generation::Vertex::Vector vertices;
+
+    // Optimze up to 4th order derivative (SNAP)
+    const int derivative_to_optimize =
+        mav_trajectory_generation::derivative_order::SNAP;
+
+    // we have 2 vertices:
+    // start = desired start vector
+    // end = desired end vector
+    mav_trajectory_generation::Vertex start(dimension), end(dimension);
+
+    /******* Configure start point *******/
+    start.makeStartOrEnd(start_pos, derivative_to_optimize);
+    start.addConstraint(mav_trajectory_generation::derivative_order::VELOCITY,
+                        start_vel);
+    vertices.push_back(start);
+
+    /******* Configure end point *******/
+    // set end point constraints to desired position and set all derivatives to zero
+    end.makeStartOrEnd(goal_pos, derivative_to_optimize);
+    end.addConstraint(mav_trajectory_generation::derivative_order::VELOCITY,
+                      goal_vel);
+    vertices.push_back(end);
+
+    // setimate initial segment times
+    std::vector<double> segment_times;
+    segment_times = estimateSegmentTimes(vertices, v_max, a_max);
+
+    // Set up polynomial solver with default params
+    mav_trajectory_generation::NonlinearOptimizationParameters parameters;
+
+    // set up optimization problem
+    const int N = 10;
+    mav_trajectory_generation::PolynomialOptimizationNonLinear<N> opt(dimension, parameters);
+    opt.setupFromVertices(vertices, segment_times, derivative_to_optimize);
+
+    // constrain velocity and acceleration
+    opt.addMaximumMagnitudeConstraint(mav_trajectory_generation::derivative_order::VELOCITY, v_max);
+    opt.addMaximumMagnitudeConstraint(mav_trajectory_generation::derivative_order::ACCELERATION, a_max);
+
+    // solve trajectory
+    opt.optimize();
+
+    // get trajectory as polynomial parameters
+    opt.getTrajectory(&(*trajectory));
+    trajectory->scaleSegmentTimesToMeetConstraints(v_max, a_max);
+    
+    return true;
+  }
+
+  void VisualServoingNode::publishTrajectory(const mav_trajectory_generation::Trajectory& trajectory) {
+    mav_planning_msgs::PolynomialTrajectory msg;
+    mav_trajectory_generation::trajectoryToPolynomialTrajectoryMsg(trajectory,
+                                                                   &msg);
+    msg.header.frame_id = "world";
+    pub_trajectory_.publish(msg);
+  }
 } //namespace geranos
 
 template<typename... Ts>
