564e322051f07babb78cc8ef44479303ab8f8e5c
TimReX-22 <tim.reinhart@bluewin.ch>
2022-06-14 11:44:39 +0200

    initialized node

--- include/geranos_visual_servoing/visual_servoing_node.h
+++ include/geranos_visual_servoing/visual_servoing_node.h
@@ -1,1 +1,33 @@
 #include <ros/ros.h>
+
+//msgs
+#include <mav_msgs/conversions.h>
+#include <mav_msgs/default_topics.h>
+#include <mav_msgs/common.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseStamped.h>
+
+namespace geranos {
+	class VisualServoingNode {
+	public:
+		VisualServoingNode(const ros::NodeHandle& nh, const ros::NodeHandle& private_nh);
+		~VisualServoingNode();
+	private:
+		void odometryCallback(const nav_msgs::OdometryConstPtr& odometry_msg);
+		void poseEstimateCallback(const geometry_msgs::PoseStamped::ConstPtr& pose_msg);
+
+		void transformPose();
+		void transformOdometry();
+
+		ros::NodeHandle nh_;
+		ros::NodeHandle private_nh_;
+
+		ros::Subscriber odometry_sub_;
+		ros::Subscriber pose_estimate_sub_;
+
+		mav_msgs::EigenOdometry current_odometry_;
+		Eigen::Vector3d current_pole_pos_;
+		Eigen::Quaterniond current_pole_rot_;
+
+	};
+}
--- src/visual_servoing_node.cpp
+++ src/visual_servoing_node.cpp
@@ -1,9 +1,39 @@
 #include <geranos_visual_servoing/visual_servoing_node.h>
 
+namespace geranos {
+  VisualServoingNode::VisualServoingNode(const ros::NodeHandle& nh, const ros::NodeHandle& private_nh) : 
+    nh_(nh),
+    private_nh_(private_nh) {
+      odometry_sub_ = nh_.subscribe(mav_msgs::default_topics::ODOMETRY, 1, &VisualServoingNode::odometryCallback, this);
+      pose_estimate_sub_ = nh_.subscribe("PolePoseNode/EstimatedPose", 1, &VisualServoingNode::poseEstimateCallback, this);
+    }
+
+  VisualServoingNode::~VisualServoingNode() {}
+
+  void VisualServoingNode::odometryCallback(const nav_msgs::OdometryConstPtr& odometry_msg) {
+    ROS_INFO_ONCE("VisualServoingNode received first odometry!");
+    mav_msgs::eigenOdometryFromMsg(*odometry_msg, &current_odometry_);
+  }
+
+  void VisualServoingNode::poseEstimateCallback(const geometry_msgs::PoseStamped::ConstPtr& pose_msg) {
+    current_pole_pos_ = mav_msgs::vector3FromPointMsg(pose_msg->pose.position);
+    current_pole_rot_ = mav_msgs::quaternionFromMsg(pose_msg->pose.orientation);
+  }
+
+} //namespace geranos
+
+template<typename... Ts>
+std::shared_ptr<geranos::VisualServoingNode> makeNode(Ts&&... params) {
+  std::shared_ptr<geranos::VisualServoingNode> Node(new geranos::VisualServoingNode(std::forward<Ts>(params)...));
+  return Node;
+}
+
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "pole_trajectory_node");
+  ros::init(argc, argv, "visual_servoing_node");
 
   ros::NodeHandle nh, private_nh("~");
+
+  auto Node = makeNode(nh, private_nh);
 
   ros::spin();
 
